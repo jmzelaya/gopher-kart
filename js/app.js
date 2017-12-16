b71da78d913e311c391cf963b65f8c04f065f028
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-12-15 23:05:07 -0500

    car now moves forward

--- js/app.js
+++ js/app.js
@@ -6,12 +6,25 @@ var mainState = {
   create: function () {
       game.stage.backgroundColor = '#72c5cf';
 
+      game.physics.startSystem(Phaser.Physics.ARCADE);
+
       this.car = game.add.sprite(50, 300, 'car');
+
+      this.physics.arcade.enable(this.car);
+
+      this.car.anchor.setTo(-0.2, 0.5);
+
+      cursors = game.input.keyboard.createCursorKeys();
   },
 
   update: function () {
+    this.car.body.velocity.x = 0;
 
-  }
+    if(cursors.right.isDown){
+      this.car.body.velocity.x = 150;
+    }
+  },
+
 };
 
 var game = new Phaser.Game(600, 400);
