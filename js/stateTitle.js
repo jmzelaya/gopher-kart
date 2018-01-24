a544e59f4c1f525db2af4445e51a1ee743af1064
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2018-01-24 13:44:52 -0500

    setting up the title screen, getting images and colors from my husband. Going to set up a scrolling background

--- js/stateTitle.js
+++ js/stateTitle.js
@@ -1,11 +1,16 @@
-// var StateTitle = {
-//   preload: function(){
-//      game.load.image("background", "assets/lanscape.png");
-//   }
+var StateTitle = {
+  preload: function(){
+     game.load.image("background", "assets/landscape.png");
+     //background fill color : #e9fffe
+  },
 
-//  create: function (){
-//    this.background = game.add.tileSprite()
-//
-//  }
-//
-// }; //END StateTitle
+  create: function (){
+     this.background = game.add.tileSprite();
+
+  },
+
+  update: function (){
+
+  },
+
+}; //END StateTitle
