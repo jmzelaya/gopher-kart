27879d4d85f2fcbea004e34eaf3c1464cc4a0037
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2018-01-24 23:10:18 -0500

    adding logo sprite to stage

--- js/stateTitle.js
+++ js/stateTitle.js
@@ -2,12 +2,13 @@ var StateTitle = {
   preload: function(){
      game.load.image("city", "assets/city-scape.png");
      game.stage.backgroundColor = 0xe9fffe;
-     game.load.spritesheet("logo", 578, 332, 24);
+     game.load.spritesheet("logo", "assets/menu-animation.png", 578, 332, 24);
   },
 
   create: function (){
      this.city = game.add.tileSprite(0, 342, 600, 90, "city");
      // this.background.autoScroll(400, 10);
+     this.logo = game.addSprite(0, 0, "logo");
 
   },
 
