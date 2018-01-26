8f6459413315553b80258e2cf0daa3a6cc1176cf
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2018-01-25 21:28:29 -0500

    added logo animations and currently playing them, it looks like a real game

--- js/stateTitle.js
+++ js/stateTitle.js
@@ -2,13 +2,16 @@ var StateTitle = {
   preload: function(){
      game.load.image("city", "assets/city-scape.png");
      game.stage.backgroundColor = 0xe9fffe;
-     game.load.spritesheet("logo", "assets/menu-animation.png", 576, 333, 28);
+     game.load.spritesheet("logo", "assets/menu-animation-flags.png", 576, 333, 28);
   },
 
   create: function (){
      this.city = game.add.tileSprite(0, 342, 600, 90, "city");
      // this.background.autoScroll(400, 10);
      this.logo = game.add.sprite(10, 10, "logo");
+     this.logo.animations.add("menu", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+      13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28], 12, true);
+     this.logo.animations.play("menu");
 
   },
 
