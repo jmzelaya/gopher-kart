0334faa9dd444c3302f6c25c89b3beaeb70fd0f5
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2018-01-26 17:09:25 -0500

    added sprite sheets for blue, pink, purple gophers, thinking of adding a 'select gopher' screen, also added psuedo code as reminders for what I need to add for the menu screen and then I'll finally start working on the game

--- js/stateTitle.js
+++ js/stateTitle.js
@@ -3,6 +3,11 @@ var StateTitle = {
      game.load.image("city", "assets/city-scape.png");
      game.stage.backgroundColor = 0xe9fffe;
      game.load.spritesheet("logo", "assets/menu-animation-flags.png", 576, 333, 28);
+     //Need to add buttons for:
+     //1. Start game
+     //2. Tutorial??
+
+     //Need: "best played in landscape view image"
   },
 
   create: function (){
@@ -12,8 +17,17 @@ var StateTitle = {
      this.logo.animations.add("menu", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
       13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28], 12, true);
      this.logo.animations.play("menu");
+     //Define and add game buttons
+     //Enable input
+     //Call setListeners();
 
   },
+
+  //Add startGame function
+  //game.state.start("StateMain");
+
+  //Define setListeners function
+  //Add listeners for correct and incorrect screen orientation
 
   update: function (){
     this.city.tilePosition.x -= 2;
