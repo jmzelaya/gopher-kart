3a8aa0d5576b36790427b323b8b8bebac3564a9b
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2018-01-25 21:21:29 -0500

    added game logo and adjust sprite hight and width :) starting to look excitingg

--- js/stateTitle.js
+++ js/stateTitle.js
@@ -2,13 +2,13 @@ var StateTitle = {
   preload: function(){
      game.load.image("city", "assets/city-scape.png");
      game.stage.backgroundColor = 0xe9fffe;
-     game.load.spritesheet("logo", "assets/menu-animation.png", 578, 332, 24);
+     game.load.spritesheet("logo", "assets/menu-animation.png", 576, 333, 28);
   },
 
   create: function (){
      this.city = game.add.tileSprite(0, 342, 600, 90, "city");
      // this.background.autoScroll(400, 10);
-     this.logo = game.addSprite(0, 0, "logo");
+     this.logo = game.add.sprite(10, 10, "logo");
 
   },
 
