5ea2ef45740484c97ad43b22739e05a555e6a505
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2018-01-30 12:03:21 -0500

    added comments and pseudo code

--- js/stateChoice.js
+++ js/stateChoice.js
@@ -1,8 +1,11 @@
 var StateChoice = {
   preload: function () {
+    //Spritesheets for the 3 colored Gophers
     game.load.spritesheet("racerBlue", "assets/gopher-blue.png", 63, 60, 7);
     game.load.spritesheet("racerPink", "assets/gopher-pink.png", 63, 60, 7);
     game.load.spritesheet("racerPurple", "assets/gopher-purple.png", 63, 60, 7);
+
+    //Add background
   },
 
   create: function () {
@@ -23,6 +26,9 @@ var StateChoice = {
     this.racerPurple.anchor.set(0.5, 0.5);
     this.racerPurple.animations.add("idle", [0, 1], 12, true);
     this.racerPurple.animations.play("idle");
+
+    //Add "buttons" for choosing racer
+    //Add "confirmation" button
   },
 
   update: function () {
--- js/stateMain.js
+++ js/stateMain.js
@@ -1,7 +1,12 @@
 var StateMain = {
 
   preload: function () {
-
+    //Add racer spritesheet
+    //Add road
+    //Add rail
+    //Add background
+    //Add other racers
+    //Add obstacles
   },
 
   create: function () {
